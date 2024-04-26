4fea9d54376b3095f7e82201cde2f87256b63af4
marcus <marcus.koch01@outlook.de>
2024-04-27 00:10:54 +0200

    feat: Make Project list accessible

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,94 +3,105 @@ import Isotope from "isotope-layout";
 import ProjectDetailsModal from "./ProjectDetailsModal";
 import { projectsData } from "./projectList";
 import HeadlineWrapperH2 from "../Header/HeadlineWrapperH2";
+import { useMemo } from "react";
 
 const Portfolio = ({ classicHeader, darkTheme }) => {
-  // init one ref to store the future isotope object
-  const isotope = useRef();
-  // store the filter keyword in a state
-  const [imagesLoaded, setimagesLoaded] = useState(0);
+  const isotope = useRef(null);
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
   const [selectedProjectDetails, setSelectedProjectDetails] = useState();
 
-  // initialize an Isotope object with configs
   useEffect(() => {
     isotope.current = new Isotope(".portfolio-filter", {
       itemSelector: ".filter-item",
       layoutMode: "masonry",
     });
 
-    // cleanup
-    return () => {
-      isotope.current.destroy();
-    };
+    return () => isotope.current.destroy();
   }, []);
 
-  // handling filter key change
   useEffect(() => {
-    if (imagesLoaded) {
-      // filterKey === "*"
-      isotope.current.arrange({ filter: `*` });
-      // : isotope.current.arrange({ filter: `.${filterKey}` });
+    if (allImagesLoaded) {
+      isotope.current.arrange({ filter: "*" });
     }
-  }, [imagesLoaded]);
+  }, [allImagesLoaded]);
+
+  const handleImageLoaded = () => {
+    // Check if all images are loaded
+    if (
+      document.querySelectorAll(".portfolio-image").length ===
+      projectsData.length
+    ) {
+      setAllImagesLoaded(true);
+    }
+  };
+
+  const handleClick = (project, event) => {
+    event.preventDefault(); // Verhindert Standardverhalten von Links
+    setSelectedProjectDetails(project);
+  };
+
+  const prjList = useMemo(
+    () =>
+      projectsData.map((project, index) => (
+        <div
+          className="col-sm-6 col-lg-4 filter-item"
+          key={index}
+          tabIndex={0}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setSelectedProjectDetails(project);
+            }
+          }}
+        >
+          <div className="portfolio-box rounded">
+            <div className="portfolio-img rounded">
+              <img
+                onLoad={handleImageLoaded}
+                className="img-fluid d-block portfolio-image"
+                src={project.thumbImage}
+                alt={project.alt}
+                style={{ maxHeight: "25vh", margin: "0 auto" }}
+              />
+              <div className="portfolio-overlay">
+                <button
+                  className="portfolio-overlay-link popup-ajax stretched-link"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  onClick={(e) => handleClick(project, e)}
+                  style={{ all: "unset", width: "100%", height: "100%" }} // Übernimmt die volle Größe des übergeordneten Elements
+                >
+                  <div className="portfolio-overlay-details">
+                    <h3
+                      className="text-white fw-400"
+                      style={{ padding: "1rem" }}
+                    >
+                      {project.title}
+                    </h3>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )),
+    []
+  );
 
   return (
     <>
       <section
         id="portfolio"
-        className={"section " + (darkTheme ? "bg-dark-2" : "bg-light")}
+        className={`section ${darkTheme ? "bg-dark-2" : "bg-light"}`}
       >
-        <div className={"container " + (classicHeader ? "" : "px-lg-5")}>
-          <HeadlineWrapperH2 label={"Portfolio"} darkTheme={darkTheme} />
-          {/* Filter Menu end */}
+        <div className={`container ${classicHeader ? "" : "px-lg-5"}`}>
+          <HeadlineWrapperH2 label="Portfolio" darkTheme={darkTheme} />
           <div className="portfolio popup-ajax-gallery">
             <div className="row portfolio-filter filter-container g-4">
-              {projectsData.map((project, index) => (
-                <div className={"col-sm-6 col-lg-4 filter-item "} key={index}>
-                  <div className="portfolio-box rounded">
-                    <div className="portfolio-img rounded">
-                      <img
-                        onLoad={() => {
-                          setimagesLoaded(imagesLoaded + 1);
-                        }}
-                        className="img-fluid d-block portfolio-image"
-                        src={project.thumbImage}
-                        alt={project.alt}
-                        style={{
-                          maxHeight: "25vh",
-                          margin: "0 auto",
-                        }}
-                      />
-                      <div className="portfolio-overlay">
-                        <a
-                          // href=""
-                          className="popup-ajax stretched-link"
-                          data-bs-toggle="modal"
-                          data-bs-target="#exampleModal"
-                          onClick={() => {
-                            setSelectedProjectDetails(projectsData[index]);
-                          }}
-                        />
-                        <div className="portfolio-overlay-details">
-                          <h3
-                            className="text-white fw-400"
-                            style={{ padding: "1rem" }}
-                          >
-                            {project.title}
-                          </h3>
-                          {/* <span className="text-light">Category</span> */}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
+              {prjList}
             </div>
           </div>
         </div>
       </section>
-      {/* <div className="project-details-modal">
-      </div> */}
-      {/* Modal */}
       <ProjectDetailsModal
         projectDetails={selectedProjectDetails}
         darkTheme={darkTheme}
