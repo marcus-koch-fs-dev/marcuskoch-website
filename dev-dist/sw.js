853de02dcfa3b395bafd350b1a00878a5aa1bc31
marcus <marcus.koch01@outlook.de>
2024-09-08 23:55:33 +0200

    feat: Theme Style Setup 1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v3fel6rg8t"
+    "revision": "0.nq3f5s16st"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import { useThemeSetter } from "./hooks/useThemeSetter";
+import { useEffect } from "react";
 
 function App() {
   const [theme] = useThemeSetter();
-
+  useEffect(() => {
+    document.body.className = theme; // Setzt die Klasse auf das body-Element
+  }, [theme]);
   return (
-    <div className={`app ${theme}`}>
+    // <div className={`app ${theme}`}>
+    <div className={`app`}>
       <Header />
       <main>
         <Outlet />
