f9e7639228a330623ce25f408664b27908880a47
marcus <marcus.koch01@outlook.de>
2024-09-08 14:42:02 +0200

    fix: App.scss incorrect capital

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.scss";
+import "./App.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
