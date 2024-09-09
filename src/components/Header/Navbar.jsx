59b27e211852bbf8b39608e973844709d6c0239a
marcus <marcus.koch01@outlook.de>
2024-09-09 11:08:26 +0200

    refactor: Improve Mobil Navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,19 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { navLinks } from "./navLinks";
 import { NavLink, Link } from "react-router-dom";
 import NavMenu from "./NavMenu";
 import useResponsiveSize from "../../hooks/useResponsiveSize";
 import "./navbar.scss";
-import { useRef } from "react";
 import ThemesToggler from "../ThemesToggler/ThemesToggler";
 
 const Navbar = () => {
   const { isMobile } = useResponsiveSize();
   const [isToggled, setIsToggled] = useState(false);
-  const menuRef = useRef(null);
 
   const renderList = () => (
-    <ul className="menu-vertical" ref={menuRef}>
+    <ul className="menu-vertical">
       {navLinks.map((link, idx) => (
         <li className="menu-item" key={idx} onClick={() => setIsToggled(false)}>
           <Link to={`/${link.to}`}>{link.name}</Link>
@@ -24,7 +22,6 @@ const Navbar = () => {
 
   const renderBar = () => (
     <>
-      {/* <ThemesToggler /> */}
       <ul className="menu-horizontal">
         {navLinks.map((link, idx) => (
           <li className="menu-item" key={idx}>
@@ -41,29 +38,29 @@ const Navbar = () => {
   );
 
   return (
-    <>
+    <div className="menu-wrapper">
       {isMobile ? (
-        <div className={`menu-wrapper-vertical`}>
+        <>
           <div className="themesToggler">
             <ThemesToggler />
           </div>
+          <div className="placeholder"></div>
           <NavMenu
-            menuRef={menuRef}
             handleClick={() => setIsToggled((prev) => !prev)}
             isToggled={isToggled}
           />
           {isToggled && renderList()}
-        </div>
+        </>
       ) : (
-        <div className="menu-wrapper-horizontal">
+        <>
           <div className="themesToggler">
             <ThemesToggler />
           </div>
           {renderBar()}
           <div className="placeholder"></div>
-        </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
