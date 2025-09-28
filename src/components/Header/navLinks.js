7d4186955e0f29c72639a50515d52390c8044b4c
Marcus K <marcus.koch01@outlook.de>
2025-09-28 19:40:00 +1000

    refactor: Disable Service page

--- src/components/Header/navLinks.js
+++ src/components/Header/navLinks.js
@@ -1,7 +1,7 @@
 export const navLinks = [
   { name: "Home", to: "", label: "Back home" },
   { name: "About Me", to: "about", label: "Details about me" },
-  { name: "What I Do", to: "services", label: "Services I offer" },
+  // { name: "What I Do", to: "services", label: "Services I offer" },
   { name: "Projects", to: "projects", label: "Projects" },
   // { name: "Rezensionen", to: "rezensionen", label: "Rezensionen" },
 ];
