eccc103d7e1d9c4523b0a7064c69f383190a2fcd
Marcus K <marcus.koch01@outlook.de>
2025-09-28 18:24:08 +1000

    refactor: Translate router links into English

--- src/components/Header/navLinks.js
+++ src/components/Header/navLinks.js
@@ -1,7 +1,7 @@
 export const navLinks = [
-  { name: "Start", to: "", label: "Zurück zum Anfang" },
-  { name: "Über mich", to: "about", label: "Über Mich" },
-  { name: "Services", to: "services", label: "Services" },
+  { name: "Home", to: "", label: "Back home" },
+  { name: "About Me", to: "about", label: "Details about me" },
+  { name: "What I Do", to: "services", label: "Services I offer" },
   { name: "Projects", to: "projects", label: "Projects" },
   // { name: "Rezensionen", to: "rezensionen", label: "Rezensionen" },
 ];
