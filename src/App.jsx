3aa2196ae733a356629f597715e8794f1c3d3f51
Marcus K <marcus.koch01@outlook.de>
2025-09-29 20:02:41 +1000

    fix: Legacy Sass commands to newer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import "/src/assets/sass/_base.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
